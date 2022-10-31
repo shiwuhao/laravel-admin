a54e4820dbef0fcc63a45464aa5dbc14df6f7aee
shiwuhao <shiwuhao@juzifenqi.com>
2022-10-31 18:36:40 +0800

    permission

---
+++ app/Models/Config.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Config extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'label',
+        'group',
+        'type',
+        'component',
+        'component_props',
+        'item',
+        'value',
+        'validate',
+        'sort',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $hidden = [
+        'deleted_at',
+        'updated_at',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $appends = [
+        'type_label',
+        'group_label',
+        'component_label',
+        'parse_value',
+//        'parse_extra',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i'
+    ];
+
+    // 配置分组
+    const GROUP_BASIC = 'basic';
+    const GROUP_SYSTEM = 'system';
+
+    const GROUP_LABEL = [
+        self::GROUP_BASIC => '基础',
+        self::GROUP_SYSTEM => '系统',
+    ];
+
+    // 数据类型
+    const TYPE_NUMBER = 'number';
+    const TYPE_STRING = 'string';
+    const TYPE_TEXT = 'text';
+    const TYPE_ARRAY = 'array';
+    const TYPE_ENUM = 'enum';
+
+    const TYPE_LABEL = [
+        self::TYPE_NUMBER => '数字',
+        self::TYPE_STRING => '字符',
+        self::TYPE_TEXT => '文本',
+        self::TYPE_ARRAY => '数组',
+        self::TYPE_ENUM => '枚举',
+    ];
+
+    // 组建类型
+    const COMPONENT_INPUT = 'input';
+    const COMPONENT_TEXTAREA = 'textarea';
+    const COMPONENT_SELECT = 'select';
+    const COMPONENT_TIME_PICKER = 'timePicker';
+    const COMPONENT_DATE_PICKER = 'datePicker';
+    const COMPONENT_DATETIME_PICKER = 'dateTimePicker';
+    const COMPONENT_UPLOAD = 'upload';
+    const COMPONENT_COLOR_PICKER = 'colorPicker';
+
+    const COMPONENT_LABEL = [
+        self::COMPONENT_INPUT => 'Input输入框',
+        self::COMPONENT_TEXTAREA => 'Textarea多行文本域',
+        self::COMPONENT_SELECT => 'Select选择器',
+        self::COMPONENT_TIME_PICKER => 'TimePicker时间选择器',
+        self::COMPONENT_DATE_PICKER => 'DatePicker日期选择器',
+        self::COMPONENT_DATETIME_PICKER => 'DateTimePicker日期时间选择器',
+        self::COMPONENT_UPLOAD => 'Upload上传',
+        self::COMPONENT_COLOR_PICKER => 'ColorPicker颜色选择器',
+    ];
+
+    /**
+     * type_label
+     * @return Attribute
+     */
+    public function typeLabel(): Attribute
+    {
+        return new Attribute(
+            get: fn() => self::TYPE_LABEL[$this->type] ?? '--',
+        );
+    }
+
+    /**
+     * group_label
+     * @return Attribute
+     */
+    public function groupLabel(): Attribute
+    {
+        return new Attribute(
+            get: fn() => self::GROUP_LABEL[$this->group] ?? '--',
+        );
+    }
+
+    /**
+     * component_label
+     * @return Attribute
+     */
+    public function componentLabel(): Attribute
+    {
+        return new Attribute(
+            get: fn() => self::COMPONENT_LABEL[$this->component] ?? '--',
+        );
+    }
+
+    public function parseValue(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->parseRule($this->type, $this->value),
+        );
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $group
+     * @return Builder
+     */
+    public function scopeOfGroup(Builder $builder, string $group = ''): Builder
+    {
+        return $builder->where('group', $group);
+    }
+
+    /**
+     * @param Builder $builder
+     * @param array $params
+     * @return Builder
+     */
+    public function scopeOfSearch(Builder $builder, array $params = []): Builder
+    {
+        if (!empty($params['title'])) {
+            $builder->where('title', 'like', "{$params['title']}%");
+        }
+
+        if (!empty($params['name'])) {
+            $builder->where('name', 'like', "{$params['name']}%");
+        }
+        return $builder;
+    }
+
+    /**
+     * @param $type
+     * @param $value
+     * @return array|false|string[]
+     */
+    protected function parseRule($type, $value): array|bool
+    {
+        switch ($type) {
+            case self::TYPE_ARRAY: // 数组
+                $array = preg_split('/[,;\r\n]+/', trim($value, ",;\r\n"));
+                if (strpos($value, ':')) {
+                    $parseValue = array();
+                    foreach ($array as $k => $val) {
+                        list($value, $label) = explode(':', $val);
+                        $parseValue[$value] = $label;
+                    }
+                } else {
+                    $parseValue = $array;
+                }
+                break;
+            default:
+                $parseValue = $value;
+        }
+        return $parseValue;
+    }
+}
---
+++ database/migrations/2022_10_31_020021_create_configs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configs', function (Blueprint $table) {
+            $table->id()->startingValue(10000);
+            $table->string('name', 50)->default('')->comment('配置标识')->unique();
+            $table->string('label', 50)->default('')->comment('配置名称');
+            $table->string('group', 50)->default('')->comment('分组');
+            $table->string('type', 50)->default('')->comment('类型');
+            $table->string('component', 50)->default('')->comment('渲染组件');
+            $table->string('component_props')->default('')->comment('渲染组件props参数');;
+            $table->string('item')->default('')->comment('配置项');
+            $table->string('value')->default('')->comment('配置值');
+            $table->string('validate')->default('')->comment('验证规则');
+            $table->unsignedInteger('sort')->default(0)->comment('排序');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configs');
+    }
+};
